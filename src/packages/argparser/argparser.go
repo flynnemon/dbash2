61b6b3c07e9ed70a0f388ae4395bb2d3b630d982
Ryan Flynn <>
2019-06-17 23:56:44 -0700

    added log viewer

--- src/packages/argparser/argparser.go
+++ src/packages/argparser/argparser.go
@@ -4,25 +4,23 @@ import (
 	"fmt"
 	"os"
 	"github.com/akamensky/argparse"
+
+	"../../models"
 )
 
-type Args struct {
-	Container     	string
-	Kubernetes		bool
-	Version			bool
-	Logs			bool
-	LogLength		int
-}
-
-func Parser() Args {
+func Parser() models.Args {
 	parser := argparse.NewParser("dbash", "Quickly gain console access to a Docker container")
 	_ArgContainer := parser.String("c", "container", &argparse.Options{Required: false, Help: "Container to connect"})
+	_ArgLogs := parser.Flag("l", "logs", &argparse.Options{Required: false, Help: "follow container logs", Default: false})
+	_ArgLogLength := parser.String("L", "log-length", &argparse.Options{Required: false, Help: "Lines to go back in the log", Default: "500"})
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
 	}
-	args := Args {
+	args := models.Args {
 		Container: *_ArgContainer,
+		LogLength: *_ArgLogLength,
+		Logs: *_ArgLogs,
 	}
 	return args
 }
